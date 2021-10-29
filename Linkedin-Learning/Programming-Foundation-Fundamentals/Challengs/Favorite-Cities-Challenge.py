ddb85625b3fc3678bc444946a4c2f97f60828446
Omar El-Shehawi <65720526+Code-Eagle-05@users.noreply.github.com>
2021-10-29 20:31:10 +0200

    Update Favorite-Cities-Challenge.py

--- Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Favorite-Cities-Challenge.py
+++ Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Favorite-Cities-Challenge.py
@@ -1,1 +1,7 @@
+# Prints out the name of a favorite city
+def favorite_city(name):
+    print("One of my favorite cities is", name)
 
+favorite_city("Tanta , Algharbyiea")
+favorite_city("Almansoura, Aldaqahlyia")
+favorite_city("Alminyia, Aldaqhlia")
