438b7f21565b2b0a35bbe88fb3cde70b6f32c150
Omar El-Shehawi <65720526+Code-Eagle-05@users.noreply.github.com>
2021-10-30 02:55:45 +0200

    Update Working-with-collections-challenge.py

--- Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Working-with-collections-challenge.py
+++ Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Working-with-collections-challenge.py
@@ -1,1 +1,23 @@
+# Third Name
 
+names = [
+    'Omar',
+    'Ahmed',
+    'Mohamed',
+    'Mahmoud',
+]
+
+print(names[3])
+
+# Translate Was 
+
+Translate = {
+    'Halo' : 'Hallo',
+    'Was' : 'What',
+    'Milich' : 'Milk',
+    'Futter' : 'Father',
+    'Hindi' : 'Mobile',
+    'Das' : 'This',
+}
+
+print(Translate['Was'])
