6b308db1f8341c35b805a3b47bcb74b71617431b
Omar El-Shehawi <65720526+Code-Eagle-05@users.noreply.github.com>
2021-10-30 02:44:30 +0200

    Update For-loop-challenge.py

--- Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/For-loop-challenge.py
+++ Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/For-loop-challenge.py
@@ -1,1 +1,14 @@
+games = [
+    'Cod',
+    'PUBG',
+    'Fortnite',
+    'amoung us',
+    'Medal Of Honor',
+    'Pes 2020',
+]
 
+
+print('My Games Are :')
+
+for games in games:
+    print(games)
